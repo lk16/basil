6e0a096cd1034104b1fcd3741f63cea7ed270269
Luuk Verweij <luuk_verweij@msn.com>
2022-03-03 20:44:35 +0100

    wip: parsing grammar

--- parser/grammar_parser.py
+++ parser/grammar_parser.py
@@ -1,5 +1,13 @@
 from enum import IntEnum, auto
-from parser.parser import OrParser, Parser, RegexBasedParser, RepeatParser, SymbolParser
+from parser.parser import (
+    ConcatenationParser,
+    LiteralParser,
+    OrParser,
+    Parser,
+    RegexBasedParser,
+    RepeatParser,
+    SymbolParser,
+)
 from typing import Dict
 
 
@@ -7,20 +15,32 @@ class GrammarSymbolType(IntEnum):
     FILE = auto()
     LINE = auto()
     COMMENT = auto()
+    WHITESPACE_LINE = auto()
+    TOKEN_DEFINITION = auto()
+    TOKEN_NAME = auto()
+    TOKEN_EXPRESSION = auto()
     WHITESPACE = auto()
-    DEFINITION = auto()
 
 
 REWRITE_RULES: Dict[IntEnum, Parser] = {
     GrammarSymbolType.FILE: RepeatParser(SymbolParser(GrammarSymbolType.LINE)),
     GrammarSymbolType.LINE: OrParser(
         SymbolParser(GrammarSymbolType.COMMENT),
-        SymbolParser(GrammarSymbolType.WHITESPACE),
-        SymbolParser(GrammarSymbolType.DEFINITION),
+        SymbolParser(GrammarSymbolType.WHITESPACE_LINE),
+        SymbolParser(GrammarSymbolType.TOKEN_DEFINITION),
     ),
     GrammarSymbolType.COMMENT: RegexBasedParser("^//[^\n]*\n"),
-    GrammarSymbolType.WHITESPACE: RegexBasedParser("^ *\n"),
-    GrammarSymbolType.DEFINITION: RegexBasedParser("^[^\n]*?\n"),
+    GrammarSymbolType.WHITESPACE_LINE: RegexBasedParser("^ *\n"),
+    GrammarSymbolType.TOKEN_DEFINITION: ConcatenationParser(
+        SymbolParser(GrammarSymbolType.TOKEN_NAME),
+        SymbolParser(GrammarSymbolType.WHITESPACE),
+        LiteralParser("="),
+        SymbolParser(GrammarSymbolType.WHITESPACE),
+        SymbolParser(GrammarSymbolType.TOKEN_EXPRESSION),
+    ),
+    GrammarSymbolType.TOKEN_NAME: RegexBasedParser("^[A-Z_]+"),
+    GrammarSymbolType.TOKEN_EXPRESSION: RegexBasedParser("^[^\n]*?\n"),
+    GrammarSymbolType.WHITESPACE: RegexBasedParser("^ *"),
 }
 
 ROOT_SYMBOL = GrammarSymbolType.FILE

32f3c620cb9568f4e710dcab86cc67c45a22fc71
Luuk Verweij <luuk_verweij@msn.com>
2022-03-05 16:26:48 +0100

    wip: adding commands

---
+++ commands.py
@@ -0,0 +1,32 @@
+import sys
+from parser.parser_generator import (
+    check_parser_staleness,
+    generate_parser,
+    regenerate_parser_if_stale,
+)
+from pathlib import Path
+
+
+def generate_parser_command() -> None:
+    if len(sys.argv) != 3:
+        print(f"Usage: {sys.argv[0]} GRAMMAR_SOURCE_FILE TARGET_PARSER_FILE")
+        exit(1)
+
+    grammar_path = Path(sys.argv[1])
+    parser_path = Path(sys.argv[1])
+
+    regenerate_parser_if_stale(grammar_path, parser_path)
+
+
+def check_parser_staleness_command() -> None:
+    if len(sys.argv) != 3:
+        print(f"Usage: {sys.argv[0]} GRAMMAR_SOURCE_FILE PARSER_FILE")
+        exit(1)
+
+    grammar_path = Path(sys.argv[1])
+    parser_path = Path(sys.argv[1])
+
+    generated_parser = generate_parser(grammar_path)
+
+    if check_parser_staleness(generated_parser, parser_path):
+        exit(1)
